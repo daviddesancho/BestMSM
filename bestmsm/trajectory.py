e8e01b778211b4c4bdba4e98e2720cec61dc973a
David De Sancho <daviddesancho@gmail.com>
2014-02-19 23:43:24 +0000

    reading trajectory file

--- bestmsm/trajectory.py
+++ bestmsm/trajectory.py
@@ -1,10 +1,7 @@
-import sys,os
-
 """ 
 ================================
  Trajectory module from BestMSM	
 ================================
- Functionalities:
 """
 
 class TimeSeries:
@@ -13,7 +10,21 @@ class TimeSeries:
 		self.readtraj()
 
 	def readtraj(self):
-	"""	Reads trajectory files assuming two column file corresponding to time
-	and state. If there are not two columns then states are read from the
-	first column and times are line numbers"""
-	
+		"""	Reads trajectory files assuming two columns: time and state. 
+		If there is only a single column, times are assumed to be the line
+		numbers """
+		raw = filter(lambda x: x[0] not in ['#','@'],
+				open(self.filename).readlines())
+		try:
+			data = map(lambda x: (x.split()[0],x.split()[1]),raw)
+			time = [float(x[0]) for x in data]
+			states = [float(x[1]) for x in data]
+		except IndexError:
+			states = [float(x[0]) for x in data]
+			time = range(len(state))
+		self.time = time
+		self.states = states
+		
+	def find_dt(self):
+		""" finds spacing between frames """
+
