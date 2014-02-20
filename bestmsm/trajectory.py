4fa2fc8acf32d8edbd377139638704db82bffc44
David De Sancho <daviddesancho@gmail.com>
2014-02-20 01:07:08 +0000

    a few functions for trajectory

--- bestmsm/trajectory.py
+++ bestmsm/trajectory.py
@@ -5,9 +5,17 @@
 """
 
 class TimeSeries:
-	def __init__(self,filename):
+	def __init__(self,filename,keys=None):
 		self.filename = filename
 		self.readtraj()
+		self.find_dt()
+		if keys == None:
+			self.find_keys()
+		else:
+			self.keys = filter(lambda x: x[0] not in ['#','@'],
+				open(keys).readlines())
+		print self.keys
+		self.traj_assign()
 
 	def readtraj(self):
 		"""	Reads trajectory files assuming two columns: time and state. 
@@ -18,13 +26,31 @@ class TimeSeries:
 		try:
 			data = map(lambda x: (x.split()[0],x.split()[1]),raw)
 			time = [float(x[0]) for x in data]
-			states = [float(x[1]) for x in data]
+			states = [x[1] for x in data]
 		except IndexError:
-			states = [float(x[0]) for x in data]
+			states = [x[0] for x in data]
 			time = range(len(state))
 		self.time = time
 		self.states = states
 		
 	def find_dt(self):
 		""" finds spacing between frames """
+		self.dt = self.time[1] - self.time[0]
 
+	def find_keys(self):
+		""" finds state names """
+		keys = []
+		for s in self.states:
+			if s not in keys:
+				keys.append(s)
+		# sort if all integers
+		if all(isinstance(x,int)==True for x in keys):
+			self.keys = keys.sort()
+		else:
+			self.keys = keys
+
+	def traj_assign(self):
+		assign = []
+		keys = self.keys
+		for s in self.states:
+			assign.append(keys.index(s))
