c6a296e20cf5930a4278f483d873a699a5ef67e3
David De Sancho <daviddesancho@gmail.com>
2014-02-21 22:57:56 +0000

    now we know the connectivity of the network

--- bestmsm/msm.py
+++ bestmsm/msm.py
@@ -5,11 +5,17 @@
 """
 import sys
 import numpy as np
+import networkx as nx
 
 class MSM:
 	def __init__(self,trajectories,lag):
+		# merge state keys from all trajectories
 		self.merge_trajs(trajectories)
-		self.count = self.calc_count(trajectories,lag=1)
+		# calculate count matrix
+		self.calc_count(trajectories,lag=1)
+		# find largest strongly connected sets
+		self.check_connect()
+		# calculate transition matrix
 
 	def merge_trajs(self,trajectories):
 		new_keys = []
@@ -36,3 +42,31 @@ class MSM:
 					idx_j = keys.index(state_j)
 				count[idx_j][idx_i] += 1
 		self.count = count
+
+	def check_connect(self):
+		""" check connectivity of rate matrix """
+		D = nx.DiGraph(self.count)
+		self.keep_states = sorted(nx.strongly_connected_components(D)[0])
+		self.keep_keys = map(lambda x: self.keys[x],keep_states)
+
+	def calc_trans(keep_states,state_keys,count):
+		""" calculate transition matrix """
+		nkeep = len(keep_states)
+		T = np.zeros([nkeep,nkeep],float)
+		n_nonzero = 0
+		neighbours = {}
+		for i in range(nkeep):
+			ni = reduce(lambda x,y:x+y, map(lambda x: count[keep_states[x]][keep_states[i]], range(nkeep)))
+			for j in range(nkeep):
+				T[j][i] = float(count[keep_states[j]][keep_states[i]])/float(ni)
+				#if count[keep_states[j]][keep_states[i]] != 0:
+				#	n_nonzero += 1
+				#	diff = difference(state_keys[keep_states[i]],state_keys[keep_states[j]])
+				#	if diff not in neighbours.keys():
+				#		neighbours[diff] = 0
+				#	neighbours[diff] += 1
+		#print "  number of nonzero elements in transition matrix = %i"%n_nonzero
+		#print "                  total size of transition matrix = %i"%(nkeep**2)
+	
+		return T,neighbours
+
