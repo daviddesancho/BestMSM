03f675971d7ff08162c9d8c9bc91b974d452ce7a
David De Sancho <daviddesancho@gmail.com>
2014-07-24 00:30:05 +0100

    getting rid of Napoleon stuff

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('sphinxext'))
 
 # -- General configuration ------------------------------------------------
 
@@ -30,7 +31,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = []
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
