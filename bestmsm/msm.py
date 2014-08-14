37c9a7b387f4a70e1431d82c09cb0a29b3e1b508
daviddesancho <daviddesancho@gmail.com>
2014-08-14 17:17:23 +0100

    set basic structure for MasterMSM --> MSM

--- bestmsm/msm.py
+++ bestmsm/msm.py
@@ -9,6 +9,60 @@ import types
 import numpy as np
 import networkx as nx
 
+class MasterMSM:
+    """
+    A class for constructing the MSM
+
+    Parameters
+    ----------
+    trajectories : list of str
+        Set of trajectories used for the construction.
+
+    lag : float
+        Lag time for building the MSM.
+
+    Attributes
+    ----------
+    keys : list of str
+        Names of states.
+
+    count : np array
+        Transition count matrix.
+
+    keep_states : list of str
+        Names of states after removing not strongly connected sets.
+        
+    """
+
+    def __init__(self, trajectories):
+        self.data = trajectories
+        self.keys = self.__merge_trajs()
+        self.dt = self.__min_dt()
+        self.__out()
+        self.__chapkol()
+
+    def __merge_trajs(self):
+        """ Merge all trajectories into a consistent set"""
+        new_keys = []
+        for traj in self.data:
+            new_keys += filter(lambda x: x not in new_keys, traj.keys)
+        return new_keys
+
+    def __min_dt(self):
+        """ Find minimum dt in trajectories"""
+        return np.min(map(lambda x: x.dt, self.data))
+
+    def __out(self):
+        """ Output description to user"""
+        print " Building MSM from "
+        print map(lambda x: x.filename, self.data)
+
+    def __chapkol(self):
+        """ Carry out Chapman-Kolmogorov test"""
+        lagtimes = self.dt*np.array([1, 2, 5, 10, 20, 50, 100, 200, 500])
+        msm = {}
+        for lagt in lagtimes:
+            msm[lagt] = MSM(self.data, lagt)
 
 class MSM:
     """
@@ -34,17 +88,11 @@ class MSM:
         Names of states after removing not strongly connected sets.
         
     """
-    def __init__(self, trajectories):
+    def __init__(self, data, lagt):
         # merge state keys from all trajectories
         self.keys = self.merge_trajs(trajectories)
         self.data = trajectories
 
-    def merge_trajs(self, trajectories):
-        """ Merge all trajectories into a consistent set. """
-        new_keys = []
-        for traj in trajectories:
-            new_keys += filter(lambda x: x not in new_keys, traj.keys)
-        return new_keys
     
     def calc_count(self, lagt=None):
         """ Calculate transition count matrix. """
@@ -113,54 +161,48 @@ class MSM:
         """ Calculate eigenvalues and eigenvectors"""
         #evalsK,rvecsK = np.linalg.eig(K)
         try: 
-            self.evalsK, self.lvecsK, self.rvecsK = 
-                scipyla.eig(self.K, left=True)
+            self.evalsK, self.lvecsK, self.rvecsK = \
+                    scipyla.eig(self.K, left=True)
         except NameError:
             print "no rate matrix"
 
-        try:
-            self.evalsT, self.lvecsT, self.rvecsT = 
+#        try:
+        self.evalsT, self.lvecsT, self.rvecsT = \
                 scipyla.eig(self.T, left=True)
+#        # sort modes
+#        elistK = []
+#        for i in range(nkeep):
+#            elistK.append([i,np.real(evalsK[i])])
+#        elistK.sort(msm_lib.esort)
+#        elistT = []
+#        for i in range(nkeep):
+#            elistT.append([i,np.real(evalsT[i])])
+#        elistT.sort(msm_lib.esort)
+#
+#        # calculate relaxation times from K and T
+#        tauK = []
+#        tauT = []
+#        for i in range(nkeep):
+#            iiK,lamK = elistK[i]
+#            if abs(lamK)>1.e-15:
+#                tauK.append(-1./lamK)
+#            else:
+#                tauK.append(0.)
+#            iT, lamT = elistT[i]
+#            if lamT>1.e-15:
+#                tauT.append(-lagt/np.log(lamT))
+#            else:
+#                tauT.append(0.)
+#
+#        # equilibrium probabilities
+#        ieqK, eK = elistK[0]
+#        peqK_sum = reduce(lambda x, y: x + y, map(lambda x: rvecsK[x,ieqK], 
+#            range(nkeep)))
+#        peqK = rvecsK[:,ieqK]/peqK_sum
+#        ieqT, eT = elistT[0]
+#        peqT_sum = reduce(lambda x,y: x + y, map(lambda x: rvecsT[x,ieqT],
+#             range(nkeep)))
+#        peqT = rvecsT[:,ieqT]/peqT_sum
+#        return tauT, peqT, rvecsT, lvecsT
+#
 
-        # sort modes
-        elistK = []
-        for i in range(nkeep):
-            elistK.append([i,np.real(evalsK[i])])
-        elistK.sort(msm_lib.esort)
-        elistT = []
-        for i in range(nkeep):
-            elistT.append([i,np.real(evalsT[i])])
-        elistT.sort(msm_lib.esort)
-
-        # calculate relaxation times from K and T
-        tauK = []
-        tauT = []
-        for i in range(nkeep):
-            iiK,lamK = elistK[i]
-            if abs(lamK)>1.e-15:
-                tauK.append(-1./lamK)
-            else:
-                tauK.append(0.)
-            iT, lamT = elistT[i]
-            if lamT>1.e-15:
-                tauT.append(-lagt/np.log(lamT))
-            else:
-                tauT.append(0.)
-
-        # equilibrium probabilities
-        ieqK, eK = elistK[0]
-        peqK_sum = reduce(lambda x, y: x + y, map(lambda x: rvecsK[x,ieqK], 
-            range(nkeep)))
-        peqK = rvecsK[:,ieqK]/peqK_sum
-        ieqT, eT = elistT[0]
-        peqT_sum = reduce(lambda x,y: x + y, map(lambda x: rvecsT[x,ieqT],
-             range(nkeep)))
-        peqT = rvecsT[:,ieqT]/peqT_sum
-        return tauT, peqT, rvecsT, lvecsT
-
-    def chapman_kolmogorov(self, lagtimes):
-        """ Carry out Chapman Kolmogorov test"""
-        for l in lagtimes:
-            self.calc_trans(lagt=l)
-            self.calc_rate(lagt=l)
-            self.calc_eigs()
