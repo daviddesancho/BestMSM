adffd44c2ec7e971e42d4a0943ae25cac991c88f
daviddesancho <daviddesancho@gmail.com>
2014-08-14 17:18:26 +0100

    example to suit new class structure

--- example/example_script.py
+++ example/example_script.py
@@ -6,19 +6,4 @@ import bestmsm.msm as msm
 
 traj_ala5 = traj.TimeSeries("ala5_32states_timeseries.dat")
 
-print " Filename : %s"%traj_ala5.filename
-print "    time between frames : %s"%traj_ala5.dt
-
-msm_ala5 = msm.MSM([traj_ala5])
-for l in [1., 2., 5., 10., 20., 50.]:
-    lagt = traj_ala5.dt*l
-    N = msm_ala5.calc_count(lagt=lagt)
-    T, keep_states, keep_keys = \
-        msm_ala5.calc_trans(count=N, lagt=lagt)
-    rate = msm_ala5.calc_rate(trans=T, lagt=lagt)
-    tauT, peqT, rvecsT, lvecsT =  msm_ala5.calc_eigs(rate=rate)
-
-
-    sys.exit()
-#print "    length of trajectory : %g"%len(traj_ala5.time)
-#print "    states : ",traj_ala5.keys
+msm_ala5 = msm.MasterMSM([traj_ala5])
