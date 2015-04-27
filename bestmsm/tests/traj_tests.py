c9a9483a7a04b37c83b51e2d72451855b7b3267e
David De Sancho <daviddesancho@gmail.com>
2015-04-27 23:51:35 +0200

    added test for keys

--- bestmsm/tests/traj_tests.py
+++ bestmsm/tests/traj_tests.py
@@ -5,17 +5,22 @@ import bestmsm.trajectory as traj
 class Ala5TimeSeriesTest():
     def setUp(self):
         traj_ala5 = traj.TimeSeries("ala5_32states_timeseries.dat")
+        state_keys = open("states", "r").readlines()
 
     def test_filename(self):
         assert self.filename == "ala5_32states_timeseries.dat"
 
     def test_data(self):
-        assert isinstance(self.dt, list)
+        assert isinstance(self.time, list)
         assert isinstance(self.states, list)
+        assert len(self.states) == len(self.time)
 
     def test_dt(self):
-        assert self.dt > 0
+        assert self.dt == 1
 
+    def test_keys(self):
+        for i in state_keys:
+            assert i in self.keys
 if __name__ == "__main__":
     unittest.main()
 
