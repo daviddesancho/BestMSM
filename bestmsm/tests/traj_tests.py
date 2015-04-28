dd2ab22204330f0562b6de7b8f0d398b74c5444a
daviddesancho <daviddesancho@gmail.com>
2015-04-28 10:44:07 +0200

    now traj tests do work

--- bestmsm/tests/traj_tests.py
+++ bestmsm/tests/traj_tests.py
@@ -4,23 +4,26 @@ import bestmsm.trajectory as traj
 
 class Ala5TimeSeriesTest(unittest.TestCase):
     def setUp(self):
-        traj_ala5 = traj.TimeSeries("ala5_32states_timeseries.dat")
-        state_keys = open("states", "r").readlines()
+        self.traj_ala5 = traj.TimeSeries("tests/ala5_32states_timeseries.dat")
+        self.state_keys = [x.split()[0] for x in open("tests/states", "r").readlines()]
 
     def test_filename(self):
-        assert self.filename == "ala5_32states_timeseries.dat"
+        assert self.traj_ala5.filename == "tests/ala5_32states_timeseries.dat"
 
     def test_data(self):
-        assert isinstance(self.time, list)
-        assert isinstance(self.states, list)
-        assert len(self.states) == len(self.time)
+        assert isinstance(self.traj_ala5.time, list)
+        assert isinstance(self.traj_ala5.states, list)
+        assert len(self.traj_ala5.states) == len(self.traj_ala5.time)
 
     def test_dt(self):
-        assert self.dt == 1
+        assert self.traj_ala5.dt == 1
 
     def test_keys(self):
-        for i in state_keys:
-            assert i in self.keys
+        print self.traj_ala5.keys
+        print self.state_keys
+        for i in self.state_keys:
+            print "testing:", i
+            assert str(i) in self.traj_ala5.keys
 
 if __name__ == "__main__":
     unittest.main()
